4f97cd5e3183690e4cc76d46a829501c81a4a6e1
N1ckorish <n1ckorish14@gmail.com>
2021-11-17 20:33:18 +1100

    added more employees and roles, added connection.js

--- db/index.js
+++ db/index.js
@@ -1,33 +1,42 @@
 const connection = require("./connection");
+
+
 class DB {
     constructor(connection){
         this.connection = connection
     };
+
     viewDepartments(){
         return this.connection.promise().query("SELECT * FROM departments;");
     };
+
     viewRoles(){
         return this.connection.promise().query("SELECT * FROM roles;");
     };
+
     viewEmployees(){
         return this.connection.promise().query("SELECT * FROM employees;");
     };
+
     createDepartment(department){
         console.log(`About to insert ${department} into department table`);
         let strQuery = `INSERT INTO departments (department_name) 
                             VALUES ("${department}");`;
         return this.connection.promise().query(strQuery);
     };
+
     createRole(role){
         console.log(`About to insert ${role} into roles table`);
         let strQuery = `INSERT INTO roles (title, salary, department_id) 
                             VALUES ("${role.role}", ${role.salary}, ${role.departmentId});`;
         return this.connection.promise().query(strQuery);
     };
+
     getManagers(){
         let strQuery = `SELECT id, first_name, last_name, role_id FROM employees WHERE manager_id IS NULL;`
         return this.connection.promise().query(strQuery);
     };
+    
     createEmployee(employee){
         let strQuery = `INSERT INTO employees (first_name, last_name, role_id, manager_id) 
                         VALUES ("${employee.first}", "${employee.last}", ${employee.roleId}, ${employee.managerId});`;
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,25 +2,45 @@ INSERT INTO departments (department_name)
 VALUES
     ("Operations"),
     ("Maketing & Advertising"),
+    ("Engineering"),
     ("Finance & Accounting"),
+    ("Legal"),
     ("IT"),
-    ("Sales") 
+    ("Sales")
 ;
     
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO roles (id, title, salary, department_id)
 VALUES 
-    ("Operations Manager", 160000, 1),
-    ("Marketing Manager", 150000, 1),
-    ("Full Stack Engineer", 150000, 1),
-    ("Marketing Analyist", 110000, 2),
-    ("Head of Sales", 105000, 2),
-    ("Operations Assistant", 95000, 3),
-    ("Sales Associate", 95000, 3),
-    ("Marketing Assistant", 95000, 3),
-    ("Junior Developer", 92000, 4)
+    (10, "Operations Manager", 160000, 1),
+    (20, "Operations Assistant", 95000, 1),
+    (30, "Marketing Manager", 150000, 2),
+    (40, "Marketing Analyist", 110000, 2),
+    (50, "Marketing Assistant", 95000, 2),
+    (60, "Software Engineer", 150000, 3),
+    (70, "Junior Developer", 90000, 3),
+    (80, "Finance Manager", 80000, 4),
+    (90, "Junior Accountant", 60000, 4),
+    (100, "Lawyer", 110000, 5),
+    (110, "Paralegal", 85000, 5),
+    (120, "Technical Support", 65000, 6),
+    (130, "Head of Sales", 105000, 7),
+    (140, "Sales Associate", 95000, 7)
 ;
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-    ("Kelvin", "Cracker", 1, 1 )
-    
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    ("Kelvin", "Cracker", 10, NULL),
+    ("Mitchel", "Trickey", 20, 1),
+    ("Zac", "Collard", 30, NULL),
+    ("Nick", "Niel", 40, 3),
+    ("Steven", "Gibbs", 50, 3),
+    ("Tiahna", "Hubbard", 60, NULL),
+    ("Jerry", "Dilger", 70, 6),
+    ("Dale", "Braniff", 80, NULL),
+    ("Kyle", "Hope", 90, 8),
+    ("Emma", "Walter", 100, 8),
+    ("James", "Mikler", 110, NULL)
+    ("Brodee", "Hoare", 120, 11)
+    ("Darcy", "Lahey", 130, NULL)
+    ("Michael", "Dun", 140, 13)
+;
    
